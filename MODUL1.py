c6624c6d288d3e2e34362ebac0d596d649b37937
Rijal97 <rizaludinbima@gmail.com>
2025-05-09 14:03:30 +0800

    MODUL1.py

---
+++ MODUL1.py
@@ -0,0 +1,28 @@
+print("TABEL KEBENARAN KONJUNGSI")
+print("-------------------------")
+print("P\t|Q\t|P AND Q")
+print("-------------------------")
+
+for P in [True,False]:
+    for Q in [True,False]:
+        Konjungsi = P and Q
+        print(f"{P}\t{Q}\t{Konjungsi}")
+        
+print("TABEL KEBENARAN DISJUNGSI")
+print("-------------------------")
+print("P\t|Q\t|P OR Q")
+print("-------------------------")
+
+for P in [True,False]:
+    for Q in [True,False]:
+        Disjungsi = P or Q
+        print(f"{P}\t{Q}\t{Disjungsi}")
+        
+print("TABEL KEBENARAN NEGASI")
+print("----------------------")
+print("P\t|NOT P")
+print("----------------------")
+
+for P in [True,False]:
+    Negasi = not P
+    print(f"{P}\t{Negasi}")
